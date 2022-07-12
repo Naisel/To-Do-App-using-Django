69c4d855d2fb5f862286505d4941c933d525b375
Naisel <naisel329@gmail.com>
2022-07-13 00:13:43 +0530

    user login

--- todoapp/todo/urls.py
+++ todoapp/todo/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path('', views.home),
-    path('home.html', views.home),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page = 'home'), name='logout'),
+    path('', views.home, name='home'),
+    path('home', views.home),
     path('user/', views.TaskList.as_view(), name='tasks'),
     # path('user/task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('user/task/<int:pk>/', views.TaskDelete.as_view(), name='task'),
     path('user/create-task/', views.TaskCreate.as_view(), name='task-create'),
     path('user/update-task/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
-
+   
 ]
--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -1,25 +1,54 @@
 
 
+# from multiprocessing import context
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect 
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from psutil import users
 
 
 from .models import Task
 
+
+
+
+
+
 # Create your views here.
+
+
+
+class CustomLoginView(LoginView):
+	template_name = 'login.html'
+	fields = '__all__'
+	redirect_authenticated_user = True
+
+	def get_success_url(self):
+		return reverse_lazy('tasks')
+
+
+
 def home(request):
     return render(request, 'home.html')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
 	model = Task
 	template_name = 'user.html'
 	context_object_name = 'tasks'
-	paginate_by = 10
+	# paginate_by = 10
 	ordering = ['-create_date']
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['tasks'] = context['tasks'].filter(users=self.request.user)
+		return context
 
 # class TaskDetail(DetailView):
 #     model = Task
@@ -28,21 +57,25 @@ class TaskList(ListView):
 #     pk_url_kwarg = 'pk'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
 	model =  Task
 	template_name = 'task.html'
 	context_object_name = 'task-create'
-	fields = '__all__'
+	fields = ['title', 'description', 'is_completed', 'create_date']
 	success_url = reverse_lazy('tasks') 
+
+	def form_valid(self, form):
+		form.instance.users = self.request.user
+		return super(TaskCreate, self).form_valid(form)
 	
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
 	model = Task
 	context_object_name = 'update'
 	template_name = 'task.html'
 	fields = '__all__'
 	success_url = reverse_lazy('tasks') 
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
 	model = Task
 	context_object_name = 'task'
 	success_url = reverse_lazy('tasks')
